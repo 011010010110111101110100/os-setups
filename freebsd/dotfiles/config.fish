d06463425095d10abcf177e72eac497c38ea5e24
Rose <56561540+011010010110111101110100@users.noreply.github.com>
2026-01-10 22:13:49 -0500

    Refactor Fish config for terminal and user paths
    Updated Fish shell configuration for terminal settings and user paths.

--- freebsd/dotfiles/config.fish
+++ freebsd/dotfiles/config.fish
@@ -2,12 +2,6 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+set -g TERM xterm-256color
+set -Ux fish_user_paths $fish_user_paths ~/.cargo/bin
 set -x GTK2_RC_FILES "$HOME/.gtkrc-2.0"
-
-# exports
-export PATH="$HOME/.local/bin:$PATH"
-export HYFETCH_DONT_WARN_RUST=1
-
-# terminal binds
-bind \x7f backward-delete-char
-bind up history-search-backward
